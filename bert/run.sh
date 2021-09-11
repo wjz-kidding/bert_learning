fb41ffc81cd3c8d57e76b035c3523ffda0d7d920
wjz-kidding <1947726673@qq.com>
2021-09-11 20:50:18 +0800

    更新了run.sh

--- bert/run.sh
+++ bert/run.sh
@@ -1,10 +1,16 @@
-export BERT_BASE_DIR=models/chinese_L-12_H-768_A-12 #全局变量 下载的预训练bert地址
-export MY_DATASET=chinese_data #全局变量 数据集所在地址
+export BERT_BASE_DIR=./bert_learning/bert/chinese_L-12_H-768_A-12
+export GLUE_DIR=../input/datanlp/nlp-data/lcqmc
 
-python run_classifier_modify2.py \
-  --data_dir=$MY_DATASET \
-  --bert_model=$BERT_BASE_DIR  \
+python run_classifier.py \
+  --task_name=sim \
   --do_train=true \
   --do_eval=true \
-  --task_name='mrpc' \
-  --output_dir=sim_output/
+  --data_dir=$GLUE_DIR/lcqmc \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --max_seq_length=128 \
+  --train_batch_size=32 \
+  --learning_rate=2e-5 \
+  --num_train_epochs=3.0 \
+  --output_dir=/tmp/mrpc_output/
